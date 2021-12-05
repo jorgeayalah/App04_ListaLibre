1e15ffc53c573b5c561e4e2f373e3eed60e280b1
jorgeayalah <A00828633@itesm.mx>
2021-12-04 19:46:41 -0600

    Initial Commit

---
+++ App04_ListaLibre/App04_ListaLibreApp.swift
@@ -0,0 +1,17 @@
+//
+//  App04_ListaLibreApp.swift
+//  App04_ListaLibre
+//
+//  Created by user194277 on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct App04_ListaLibreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App04_ListaLibre/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App04_ListaLibre
+//
+//  Created by user194277 on 12/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
